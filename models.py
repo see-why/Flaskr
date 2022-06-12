c8c95d82972e438441b48f683cf8a9fa2c5a967f
see-why <iyadicy@gmail.com>
2022-06-12 08:06:53 +0100

    define plants class

--- models.py
+++ models.py
@@ -19,3 +19,44 @@ def setup_db(app, database_path=database_path):
 
 
 """Plant class"""
+
+
+class Plant(db.Model):
+    __tablename__ = "plants"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    scientific_name = Column(String)
+    is_poisonous = Column(Boolean)
+    primary_color = Column(String)
+
+
+    def __init__(self, name, scientific_name, is_poisonous, primary_color):
+        self.name = name
+        self.scientific_name = scientific_name
+        self.is_poisonous = is_poisonous
+        self.primary_color = primary_color
+
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    def update(self):
+        db.session.commit()
+
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+    def format(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "scientific_name": self.scientific_name,
+            "is_poisonous": self.is_poisonous,
+            "primary_color": self.primary_color,
+        }
